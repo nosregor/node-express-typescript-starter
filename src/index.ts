dfb6532d8aadb142342e6940dc14ee737d71004f
Andrew Rogerson <andrew.rogerson@free-now.com>
2021-01-26 12:30:36 +0100

    add linters and prettier rules

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 const PORT = 8000;
 
-app.get("/", (req, res) => res.send("Express + TypeScript Server"));
+app.get('/', (req, res) => res.send('Express + TypeScript Server'));
 
 app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+  console.log(
+    `⚡️[server]: Server is running at https://localhost:${PORT}`,
+  );
 });
